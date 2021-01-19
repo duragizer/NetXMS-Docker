48c5ac4c399ad11589b71d36bb102124816104a6
duragizer <halmsjo@gmail.com>
2021-01-19 15:03:46 +0100

    Initial

---
+++ web/docker-entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "server=$NETXMS_SERVER\nsessionTimeout=$NETXMS_SESSIONTIMEOUT\nenableAdvancedSettings=$NETXMS_ENABLEADVANCEDSETTINGS" >/var/lib/jetty/resources/nxmc.properties
+echo "netxms.server.address=$NETXMS_SERVER\nnetxms.server.port=$WEBAPI_PORT\nnetxms.server.useEncryption=$WEBAPI_USEENCRYPTION" >/var/lib/jetty/resources/nxapisrv.properties
